8de4e5e56783b2ef4c8cdbb60d2d4443d2df224c
jrbudda <jrbudda@gmail.com>
2017-09-22 13:59:09 -0400

    1122 fix some thigns

--- src/com/mtbs3d/minecrift/asm/ObfNames.java
+++ src/com/mtbs3d/minecrift/asm/ObfNames.java
@@ -1,8 +1,8 @@
 package com.mtbs3d.minecrift.asm;
 
 public class ObfNames {
-	public static final String GUICONTAINER = "bme";
-	public static final String GUICONTAINERCREATIVE = "bmn";
-	public static final String GLSTATEMANAGER = "buq";
-	public static final String SLOT = "agp";
+	public static final String GUICONTAINER = "bmg";
+	public static final String GUICONTAINERCREATIVE = "bmp";
+	public static final String GLSTATEMANAGER = "bus";
+	public static final String SLOT = "agr";
 }
