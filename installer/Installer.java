a03961da6b535145162602e4d2d538c79faecc33
Techjar <tecknojar@gmail.com>
2018-05-26 18:10:38 -0400

    Make this https

--- installer/Installer.java
+++ installer/Installer.java
@@ -80,7 +80,7 @@ public class Installer extends JPanel  implements PropertyChangeListener
 	private String[] forgeVersions = null;
 	private boolean forgeVersionInstalled = false;
 	private static String FULL_FORGE_VERSION = MINECRAFT_VERSION + "-" + FORGE_VERSION;
-	private String forge_url = "http://files.minecraftforge.net/maven/net/minecraftforge/forge/" + FULL_FORGE_VERSION + "/forge-" + FULL_FORGE_VERSION + "-installer.jar";
+	private String forge_url = "https://files.minecraftforge.net/maven/net/minecraftforge/forge/" + FULL_FORGE_VERSION + "/forge-" + FULL_FORGE_VERSION + "-installer.jar";
 	private File forgeInstaller;
 	private JTextField selectedDirText;
 	private JLabel infoLabel;
